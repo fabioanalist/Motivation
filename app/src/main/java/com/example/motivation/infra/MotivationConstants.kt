202ddf21ae9436681fd0750f533a1da850ef1a0e
fabioanalist <fabio@ifabio.combr>
2020-03-10 14:07:00 -0300

    App Kotlin

---
+++ app/src/main/java/com/example/motivation/infra/MotivationConstants.kt
@@ -0,0 +1,11 @@
+package com.example.motivation.infra
+
+class  MotivationConstants private constructor(){
+
+    object KEY {
+        val PERSON_NAME = "name"
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/motivation/infra/SecurityPreferences.kt
@@ -0,0 +1,25 @@
+package com.example.motivation.infra
+
+import android.content.Context
+import android.content.SharedPreferences
+import java.nio.channels.spi.AbstractSelectionKey
+
+
+class SecurityPreferences(context: Context) {
+
+    private val mSharedPreferences = context.getSharedPreferences("motivation", Context.MODE_PRIVATE)
+
+    fun storeString( key: String, value: String) {
+        mSharedPreferences.edit().putString(key, value).apply()
+    }
+
+    fun getString(key: String) : String {
+        return mSharedPreferences.getString(key, "") ?: ""
+    }
+
+    fun storeString(key: String) {
+
+    }
+
+
+}
